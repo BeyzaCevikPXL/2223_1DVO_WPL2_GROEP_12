8b1b952523921ab408617fdf93d9387a60c66497
Nathan-Schoeters <129076634+NathanSchoeters@users.noreply.github.com>
2023-05-23 09:56:49 +0200

    werkende shoppingcart

--- shoppingCart.js
+++ shoppingCart.js
@@ -20,102 +20,157 @@ let cartItems = [
     }
 ];
 
-// Elke button selecteren met de zelfde klasse (ButtonClick). Deze allemaal samen oproepen. Na oproepen: foreach over buttons.
-// Voor elke button in de for each een event listener. event on click. event.target.dataset.waarde. addItem(waarde);
-
 //Variabelen
 let button = document.querySelectorAll(".buttonClick");
 
 button.forEach((btn) => {
     btn.addEventListener('click', event => {
         let waarde = event.target.dataset.waarde;
+        // debugger;
         addItem(waarde);
     })
 });
-
-//LOCAL STORAGE!!
-// addItem RemoveIten en display Item
-
-// if button.waarde == id -> voeg item toe aan winkelwagen
 
 let Cart=[];
 
 //Add Item
 function addItem(waarde){
-    for(let i = 0; i < shoppingCart.length; i++){
-        if(Cart.id[i] === waarde){
-            //Als dit waar is staat het item al in de winkel kar
+    for(let i = 0; i < Cart.length; i++){
+        if(Cart[i].id === waarde){
+            //Als dit waar is staat het item al in de winkelkar
             return;
         }
     }
     Cart.push(cartItems[waarde]);
-    shoppingCart()
+   displayShoppingCart()
 }
 
 
 //remove Item
-function removeItem(){
+function removeItem(waarde) {
+    waarde = waarde * 1;
+    console.log("remove Item:--" + waarde + "--");
 
+    for (let i = 0; i < Cart.length; i++) {
+        if (Cart[i].id === waarde) {
+            Cart.splice(i, 1)
+        }
+    }
+    displayShoppingCart()
 }
-
 function total() {
-
+    let totaal = 0;
+    let korting = 0;
+    for(let i = 0; i < Cart.length; i++){
+        totaal += Cart[i].prijs;
+    }
+    if (Cart.length > 1){
+        korting = (totaal/10);
+    }
+    totalePrijs = "€" + (totaal-korting);
+    document.getElementById("standaardPrijs").innerHTML = totaal;
+    document.getElementById("korting").innerHTML = "-" + korting;
+    document.getElementById("totaalPrijs").innerHTML = totalePrijs;
 }
 
 
 //Display Item
-function shoppingCart(){
-    // elements createn
-    let div_Row1 = document.createElement("div");
-    let div_Row1_Col1 = document.createElement("div");
-    let div_Row1_Col2 = document.createElement("div");
+function displayShoppingCart(){
 
-    let product_Naam = document.createElement("h4");
-    let knop = document.createElement("button");
-    let kruisje = document.createElement("i");
+    let cart_html = document.getElementById("shoppingCart");
 
-    let uitleg = document.createElement("p");
-    let prijs = document.createElement("h4");
+    if(cart_html) {
+        cart_html.innerHTML = "";
 
-    let div_Row2 = document.createElement("div");
-    let div_Row2_Col1 = document.createElement("div");
-    let div_Row2_Col2 = document.createElement("div");
+        // if (Cart.length === 0) {
+        //     let uitleg = document.createElement("h4")
+        //     let uitlegTekst = "Er zitten nog geen items in je winkelmandje!";
+        //     uitleg.innerHTML = uitlegTekst;
+        //
+        //     cart_html.appendChild(div_Row1);
+        // }
+        // else{
+        for (let i = 0; i < Cart.length; i++) {
 
-    // input van de elements
-    let product_Naam_tekst = cartItems.titel[waarde];
-    let uitleg_tekst = cartItems.text[waarde];
-    let prijs_tekst = cartItems.prijs[waarde];
+            // elements createn
+            let div_Row1 = document.createElement("div");
+            let div_Row1_Col1 = document.createElement("div");
+            let div_Row1_Col2 = document.createElement("div");
 
-    // op alle elements values toevoegen
-    product_Naam.appendChild(product_Naam_tekst);
-    uitleg.appendChild(uitleg_tekst);
-    prijs.appendChild(prijs_tekst);
+            let product_Naam = document.createElement("h4");
+            let knop = document.createElement("button");
+            let kruisje = document.createElement("i");
 
-    // alle classes (op de elements) & events bv remove button moet id hebbe en die moet for each hebbe om door te gaan
-    div_Row1.classList.add('row');
-    div_Row2.classList.add('row');
+            kruisje.dataset.waarde = Cart[i].id;
 
-    div_Row1_Col1.classList.add('col-8');
-    div_Row1_Col2.classList.add('col-4');
-    div_Row1_Col2.classList.add('right');
+            knop.addEventListener('click', function (e) {
+                removeItem(e.target.dataset.waarde);
+            });
 
-    div_Row2_Col1.classList.add('col-8');
-    div_Row2_Col2.classList.add('col-4');
-    div_Row2_Col2.classList.add('right');
+            let uitleg = document.createElement("p");
+            let prijs = document.createElement("h4");
 
-    kruisje.classList('fa-solid');
-    kruisje.classList('fa-x');
+            let div_Row1_Col3 = document.createElement("div");
+            let div_Row1_Col4 = document.createElement("div");
 
-    // alle elementen op elkaar appenden. (bv: li op de ul, ul op de div etc)
-    knop.appendChild('kruisje');
-    div_Row1_Col2.appendChild('knop');
-    div_Row1_Col1.appendChild('product_Naam');
-    div_Row1.appendChild('div_Row1_Col1');
-    div_Row1.appendChild('div_Row1_Col2');
+            // input van de elements
+            let product_Naam_tekst = Cart[i].titel;
+            let uitleg_tekst = Cart[i].text;
+            let prijs_tekst = "€" + Cart[i].prijs;
 
-    div_Row2_Col1.appendChild('uitleg');
-    div_Row2_Col2.appendChild('prijs');
-    div_Row2.appendChild('div_Row2_Col1');
-    div_Row2.appendChild('div_Row2_Col2');
+            //debugger;
 
+            // op alle elements values toevoegen
+            product_Naam.innerHTML = product_Naam_tekst;
+            uitleg.innerHTML = uitleg_tekst;
+            prijs.innerHTML = prijs_tekst;
+
+
+            // alle classes (op de elements) & events bv remove button moet id hebbe en die moet for each hebbe om door te gaan
+            div_Row1.classList.add('row');
+
+            div_Row1_Col1.classList.add('col-8');
+            div_Row1_Col2.classList.add('col-4');
+            div_Row1_Col2.classList.add('right');
+
+            div_Row1_Col3.classList.add('col-8');
+            div_Row1_Col4.classList.add('col-4');
+            div_Row1_Col4.classList.add('right');
+
+            kruisje.classList.add('fa-solid');
+            kruisje.classList.add('fa-x');
+
+            // alle elementen op elkaar appenden. (bv: li op de ul, ul op de div etc)
+            knop.appendChild(kruisje);
+            div_Row1_Col2.appendChild(knop);
+            div_Row1_Col1.appendChild(product_Naam);
+            div_Row1.appendChild(div_Row1_Col1);
+            div_Row1.appendChild(div_Row1_Col2);
+
+            div_Row1_Col3.appendChild(uitleg);
+            div_Row1_Col4.appendChild(prijs);
+            div_Row1.appendChild(div_Row1_Col3);
+            div_Row1.appendChild(div_Row1_Col4);
+
+            //Add everything
+            cart_html.appendChild(div_Row1);
+
+        }
+        // }
+    }
+        total();
+        saveCart();
 }
+
+function loadCart(){
+    if(localStorage.getItem("Cart")){
+        Cart = JSON.parse(localStorage.getItem("Cart"));
+    }
+}
+
+function saveCart(){
+    localStorage.setItem("Cart", JSON.stringify(Cart));
+}
+
+loadCart();
+displayShoppingCart();
