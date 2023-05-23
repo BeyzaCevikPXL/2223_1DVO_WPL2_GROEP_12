9326c58ce97662748c1c6acbf865ed2448b80006
BeyzaCevikPXL <beyza.cevik@student.pxl.be>
2023-05-23 10:26:23 +0200

    loading js toegevoegd

---
+++ loading.js
@@ -0,0 +1,5 @@
+//eerst alles inladen, dan pas showen//
+window.addEventListener("load", function() {
+    var container = document.getElementById("container");
+    container.style.visibility = "visible";
+})
