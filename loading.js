febc3fb98adb2e3581d2fa35c17e69dcdc1474ca
BeyzaCevikPXL <beyza.cevik@student.pxl.be>
2023-05-29 14:08:08 +0200

    java load toegevoegd

--- loading.js
+++ loading.js
@@ -1,5 +1,8 @@
-//eerst alles inladen, dan pas showen//
-window.addEventListener("load", function() {
-    var container = document.getElementById("container");
-    container.style.visibility = "visible";
-})
+// Hide the body element initially
+document.body.style.display = 'none';
+
+// Triggered when the entire page is loaded, including styles, images, and other resources
+window.onload = function() {
+    // Display the body element when everything is loaded
+    document.body.style.display = 'block';
+};
